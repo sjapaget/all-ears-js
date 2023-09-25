6bebc6f999721cedcddf4ca42d5dde9550093f6f
theodore <theodore1712@yahoo.fr>
2023-09-25 17:09:09 +0200

    Functional event handlers for +/- buttons (nb of players/rounds)

--- src/js/handlers/players-screen-handler.js
+++ src/js/handlers/players-screen-handler.js
@@ -1,10 +1,23 @@
 export function changeNbOfPlayers(event){
+	if(event.target.id != 'add' && event.target.id != 'remove'){
+		alert('nope')
+		return;
+	}
+
+	// DRY it up (lines 12 to end: add 'nbOf...' as parameter for add/remove functions)
+
 	let nbOfPlayers = document.getElementById('nbOfPlayers');
+	let nbOfRounds = document.getElementById('nbOfRounds');
 
-	if(event.target.id == 'add') addPlayer();
-	else if(event.target.id == 'remove') removePlayer();
+	if(event.target.parentElement.children[1].id == 'nbOfPlayers'){
+		if(event.target.id =='add') addPlayer();
+		if(event.target.id =='remove') removePlayer();
+	}
 
-	else return;
+	if(event.target.parentElement.children[1].id == 'nbOfRounds'){
+		if(event.target.id =='add') addRound();
+		if(event.target.id =='remove') removeRound();
+	}
 }
 
 function addPlayer(){
@@ -17,5 +30,15 @@ function addPlayer(){
 
 function removePlayer(){
 	if(+nbOfPlayers.textContent > 3) nbOfPlayers.textContent = +nbOfPlayers.textContent - 1;
+	else return;
+}
+
+function addRound(){
+	if(+nbOfRounds.textContent < 10) nbOfRounds.textContent = +nbOfRounds.textContent + 1;
+	else return;
+}
+
+function removeRound(){
+	if(+nbOfRounds.textContent > 3) nbOfRounds.textContent = +nbOfRounds.textContent - 1;
 	else return;
 }
